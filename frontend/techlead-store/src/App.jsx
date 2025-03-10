25bba2943abe39960bd64c1a7fd306bca2be17a0
yoruwitch <evelyn.fernandes4@gmail.com>
2025-03-10 20:44:48 -0300

    feat: initialize React project with Vite, add basic structure and dependencies

---
+++ frontend/techlead-store/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@chakra-ui/react";
+import React from "react";
+
+function App() {
+    return (
+        <>
+            <Button>Oi</Button>
+        </>
+    );
+}
+
+export default App;
